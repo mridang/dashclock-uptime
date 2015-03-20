3fd92d3f5fe60f5836aaf394bfc702ebda4c5588
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2015-03-20 10:22:14 +0200

    Initial commit

---
+++ src/main/java/com/mridang/uptime/UptimeWidget.java
@@ -0,0 +1,111 @@
+package com.mridang.uptime;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+import org.acra.ACRA;
+import org.ocpsoft.prettytime.Duration;
+import org.ocpsoft.prettytime.PrettyTime;
+import org.ocpsoft.prettytime.units.JustNow;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.SystemClock;
+import android.provider.Settings;
+import android.util.Log;
+
+import com.google.android.apps.dashclock.api.ExtensionData;
+
+/*
+ * This class is the main class that provides the widget
+ */
+public class UptimeWidget extends ImprovedExtension {
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.mridang.battery.ImprovedExtension#getIntents()
+	 */
+	@Override
+	protected IntentFilter getIntents() {
+		return null;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.mridang.battery.ImprovedExtension#getTag()
+	 */
+	@Override
+	protected String getTag() {
+		return getClass().getSimpleName();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.mridang.battery.ImprovedExtension#getUris()
+	 */
+	@Override
+	protected String[] getUris() {
+		return null;
+	}
+
+	/*
+	 * @see
+	 * com.google.android.apps.dashclock.api.DashClockExtension#onUpdateData
+	 * (int)
+	 */
+	@Override
+	protected void onUpdateData(int intReason) {
+
+		Log.d(getTag(), "Calculating the phone's uptime");
+		ExtensionData edtInformation = new ExtensionData();
+		setUpdateWhenScreenOn(true);
+
+		try {
+
+			PrettyTime pd = new PrettyTime(new Date(SystemClock.elapsedRealtime()));
+			List<Duration> lstDurations = pd.calculatePreciseDuration(new Date(0));
+			for (Duration d : lstDurations) {
+
+				if (d.getUnit() instanceof JustNow) {
+					lstDurations.remove(d);
+				}
+
+			}
+
+			if (!lstDurations.isEmpty()) {
+
+				SimpleDateFormat sdf = new SimpleDateFormat("dd MMMM, yyyy", Locale.getDefault());
+				Date datReboot = new Date(System.currentTimeMillis() - SystemClock.elapsedRealtime());
+
+				edtInformation.clickIntent(new Intent(Settings.ACTION_DEVICE_INFO_SETTINGS));
+				edtInformation.expandedBody(getString(R.string.message, pd.format(lstDurations)));
+				edtInformation.status(getString(R.string.status, sdf.format(datReboot)));
+				edtInformation.status(getString(R.string.status, sdf.format(datReboot)));
+				edtInformation.visible(true);
+
+			}
+
+		} catch (Exception e) {
+			edtInformation.visible(false);
+			Log.e(getTag(), "Encountered an error", e);
+			ACRA.getErrorReporter().handleSilentException(e);
+		}
+
+		edtInformation.icon(R.drawable.ic_dashclock);
+		doUpdate(edtInformation);
+
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see com.mridang.alarmer.ImprovedExtension#onReceiveIntent(android.content.Context, android.content.Intent)
+	 */
+	@Override
+	protected void onReceiveIntent(Context ctxContext, Intent ittIntent) {
+		onUpdateData(UPDATE_REASON_MANUAL);
+	}
+
+}
